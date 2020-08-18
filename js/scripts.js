784d1d2d68196f9520f69c4330e273c6293690de
Mike Manchee <mikemanchee@gmail.com>
2020-08-18 12:48:37 -0700

    redesign project to create a stress test with the project
    co-authored-by: David McCaslin <davidmccaslin94@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,87 @@
+
+// function stressTest(stress, health, manage) {
+//   let result;
+//   let numStress = 0;
+//   let numHealth = 0;
+//   let numManage = 0;
+//   stress.forEach(function(num) {
+//     numstress += num;
+//   })
+
+//   health.forEach(function(num) {
+//     numhealth += num;
+//   })
+
+//   manage.forEach(function(num) {
+//     numManage += num;
+//   })
+
+//   if (numStress > 3 && numHealth > 3) {
+//     if (numManage > 2) {
+//       result = "You are doing a good job at managing your stress level."
+//     } else if (numManage > 1) {
+//       result = "Here are some additional resources to help you manage."
+//     } else { 
+//       result = "you should look into some options to reduce your stress."
+//     }
+//   } else if (numStress > 1 && numHealth > 1) {
+//     if (numManage > 2) {
+//       result = "You are doing a good job at managing your stress level."
+//     } else {
+//       result = "You should should be fine."
+//     } 
+//   } else {
+//     result = "Sounds like you're pretty well adjusted."
+//   }
+//   return result;
+// }
+
+
+
+function stressTest(stress, health, manage) {
+  let result;
+  if (stress.length > 3 && health.length > 3) {
+    if (manage.length > 2) {
+      result = "You are doing a good job at managing your stress level.";
+    } else if (manage.length > 1) {
+      result = "Here are some additional resources to help you manage.";
+    } else { 
+      result = "you should look into some options to reduce your stress.";
+    }
+  } else if (stress.length > 1 && health.length > 1) {
+    if (manage.length > 2) {
+      result = "You are doing a good job at managing your stress level.";
+    } else {
+      result = "You should should be fine.";
+    } 
+  } else {
+    result = "Sounds like you're pretty well adjusted.";
+  }
+  return result;
+}
+
 $(document).ready(function(){
-  $("form#transportation_survey").submit(function(event){
+  $("form#stress-test").submit(function(event){
     event.preventDefault();
-    $("#work-responses").show();
-    $("input:checkbox[name=work-transportation]:checked").each(function(){
-      const workTransportationMode = $(this).val();
-      $('#work-responses').append(workTransportationMode + "<br>");
+    let negStresser = [];
+    let negHealth = [];
+    let manage = [];
+
+    $("input:checkbox[name=stress-signs]:checked").each(function(){
+      negStresser.push($(this).val());
     });
-    $("#fun-responses").show();
-    $("input:checkbox[name=fun-transportation]:checked").each(function(){
-      const funTransportationMode = $(this).val();
-      $('#fun-responses').append(funTransportationMode + "<br>");
+    $("input:checkbox[name=stress-symptoms]:checked").each(function(){
+      negHealth.push($(this).val());
     });
-    $('#transportation_survey').hide();
+    $("input:checkbox[name=stress-methods]:checked").each(function(){
+      manage.push($(this).val());
+    });
+
+    const testResult = stressTest(negStresser, negHealth, manage);
+
+    $("#result").text(testResult);
+    
+    $('#stress-test').hide();
 
     $("#results").show();
   });
